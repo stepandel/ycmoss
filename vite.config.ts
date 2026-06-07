cc0603bc72ba842df9a51594a18ef0bafb374832
stepandel <stepandel@gmail.com>
2026-06-07 10:16:40 -0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": "http://127.0.0.1:8787"
+      "/api": "http://127.0.0.1:8787",
+      "/ws": {
+        target: "ws://127.0.0.1:8787",
+        ws: true
+      }
     }
   }
 });
